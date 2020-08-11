80506c31def3c5a7f30f4542390d1292b1d99736
ElijahTobs <eligenius@gmail.com>
2020-08-11 16:33:16 +0100

    Migrate db

--- db/migrate/20200811150601_create_posts.rb
+++ db/migrate/20200811150601_create_posts.rb
@@ -3,8 +3,9 @@ class CreatePosts < ActiveRecord::Migration[5.2]
     create_table :posts do |t|
       t.string :title
       t.text :body
-      t.references :user_id, foreign_key: true
+      t.references :user, foreign_key: true
 
+      # remove_column :user_id_id
       t.timestamps
     end
   end
