1a7b696de56a0c259141bd090abad3f7938aa914
ElijahTobs <eligenius@gmail.com>
2020-08-13 01:45:44 +0100

    Add validation for body

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
+
   validates :user_id, presence: true
+  validates :body, presence: true
   validates :title, length: { in: 10..30 }
 end
